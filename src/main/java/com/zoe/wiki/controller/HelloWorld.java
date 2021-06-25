62becc4429ec493b81e1229232d9d730849a573d
zoe <zoeeying@gmail.com>
2021-06-26 00:29:04 +0800

    HelloWorld接口

---
+++ src/main/java/com/zoe/wiki/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zoe.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
