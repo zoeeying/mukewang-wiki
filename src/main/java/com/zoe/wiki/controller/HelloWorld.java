42a27bfdc999aa91698137f62408b1a4935cf38b
zoe <zoeeying@gmail.com>
2021-06-27 00:03:14 +0800

    接口测试

--- src/main/java/com/zoe/wiki/controller/HelloWorld.java
+++ src/main/java/com/zoe/wiki/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.zoe.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,12 @@ public class HelloWorld {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "GET: Hello World";
+    }
+
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "POST: Hello " + name;
     }
 }
