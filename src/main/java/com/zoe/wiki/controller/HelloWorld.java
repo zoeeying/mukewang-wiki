3b41d5e808f52b64fb36bf6ed17bdabefb7be464
zoe <zoeeying@gmail.com>
2021-06-27 09:34:40 +0800

    配置文件

--- src/main/java/com/zoe/wiki/controller/HelloWorld.java
+++ src/main/java/com/zoe/wiki/controller/HelloWorld.java
@@ -1,11 +1,15 @@
 package com.zoe.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+    // 读取自定义配置项，并赋值给testHello
+    @Value("${test.hello:ZOE}")
+    private String testHello;
 
     @GetMapping("/hello")
     public String hello(){
