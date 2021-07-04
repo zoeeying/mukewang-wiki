bd6d3e25fe14012fe0da765a3f1cc6a4413f7e3e
zoe <zoeeying@gmail.com>
2021-07-04 19:17:23 +0800

    全局使用@ant-design/icons-vue图标；使用List组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import * as Icons from '@ant-design/icons-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).use(Antd).mount('#app')
+
+// 全局使用图标
+const icons: any = Icons
+for(const i in icons){
+    app.component(i, icons[i])
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,7 +46,38 @@
       </a-menu>
     </a-layout-sider>
     <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-      Content
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
@@ -55,9 +86,35 @@
   import { defineComponent, onMounted, ref, reactive, toRef } from 'vue'
   import axios from 'axios'
 
+  const listData: Record<string, string>[] = [];
+
+  for (let i = 0; i < 23; i++) {
+    listData.push({
+      href: 'https://www.antdv.com/',
+      title: `ant design vue part ${i}`,
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+  }
+
   export default defineComponent({
     name: 'Home',
     setup() {
+      const pagination = {
+        onChange: (page: number) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      };
+      const actions: Record<string, string>[] = [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ];
+
       // 使用ref和reactive实现数据绑定
       const ebooks = ref() // 响应式变量，最后需要return出去
       const ebooks2 = reactive({ books: [] })
@@ -71,7 +128,10 @@
 
       return {
         ebooks,
-        ebooks2: toRef(ebooks2, 'books') // 转换成响应式变量
+        ebooks2: toRef(ebooks2, 'books'), // 转换成响应式变量
+        listData,
+        pagination,
+        actions,
       }
     }
   })
