dbc11819982d17264b41b1acb460d706f2022d3b
zoe <zoeeying@gmail.com>
2021-07-18 20:37:29 +0800

    电子书管理表格展示

--- doc/dev.sql
+++ doc/dev.sql
@@ -14,8 +14,8 @@ create table `ebook` (
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='电子书';
 
-insert into `ebook` (id, name, description) values (1, 'Spring Boot 入门教程', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (2, 'Vue 入门教程', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, cover, description) values (1, 'Spring Boot 入门教程', '/image/cover1.png', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, cover, description) values (2, 'Vue 入门教程', '/image/cover2.png', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
 insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,117 @@
 <template>
-  <div class="about">
-    <h1>电子书管理</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <a-table
+        :columns="columns"
+        :row-key="record => record.id"
+        :data-source="ebooks"
+        :pagination="pagination"
+        :loading="loading"
+        @change="onTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img :src="cover" alt="avatar" v-if="cover" :style="{ width: 50 }">
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary">
+              编辑
+            </a-button>
+            <a-button type="danger">
+              删除
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
 </template>
+
+<script lang="ts">
+  import { defineComponent, onMounted, ref } from 'vue'
+  import axios from 'axios'
+
+  export default defineComponent({
+    name: 'AdminEbook',
+    setup(){
+      const ebooks = ref()
+      const pagination = ref({
+        current: 1,
+        pageSize: 2,
+        total: 0
+      })
+      const loading = ref(false)
+
+      const columns = [
+        {
+          title: '封面',
+          dataIndex: 'cover',
+          slots: { customRender: 'cover'}
+        },
+        {
+          title: '名称',
+          dataIndex: 'name'
+        },
+        {
+          title: '分类一',
+          key: 'category1Id',
+          dataIndex: 'category1Id'
+        },
+        {
+          title: '分类二',
+          dataIndex: 'category2Id'
+        },
+        {
+          title: '文档数',
+          dataIndex: 'docCount'
+        },
+        {
+          title: '阅读数',
+          dataIndex: 'viewCount'
+        },
+        {
+          title: '点赞数',
+          dataIndex: 'voteCount'
+        },
+        {
+          title: 'Action',
+          key: 'action',
+          slots: { customRender: 'action'}
+        }
+      ]
+
+      // 数据查询
+      const onQuery = async (params: any) => {
+        loading.value = true
+        const res = await axios.get('/ebook/list', params) 
+        loading.value = false
+        const data = res.data
+        ebooks.value = data.content
+
+        // 重置分页
+        pagination.value.current = params.page
+      }
+
+      // 表格页码变更
+      const onTableChange = (pagination: any) => {
+        onQuery({
+          page: pagination.current,
+          size: pagination.pageSize
+        })
+      }
+
+      onMounted(() => {
+        onQuery({})
+      })
+
+      return {
+        ebooks,
+        pagination,
+        columns,
+        loading,
+        onTableChange
+      }
+    },
+  })
+
+</script>
