856d5b4d9088de9d6b1b447264a6eb6906870aca
zoe <zoeeying@gmail.com>
2021-07-18 21:23:41 +0800

    集成PageHelper插件用于分页，将mapper日志级别改成trace

--- src/main/java/com/zoe/wiki/service/EbookService.java
+++ src/main/java/com/zoe/wiki/service/EbookService.java
@@ -1,21 +1,26 @@
 package com.zoe.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zoe.wiki.domain.Ebook;
 import com.zoe.wiki.domain.EbookExample;
 import com.zoe.wiki.mapper.EbookMapper;
 import com.zoe.wiki.req.EbookReq;
 import com.zoe.wiki.resp.EbookResp;
 import com.zoe.wiki.utils.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class EbookService {
+    // 打印日志
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
@@ -27,7 +32,14 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        // 只对下面第一个select语句起作用
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数: {}", pageInfo.getTotal());
+        LOG.info("总页数: {}", pageInfo.getPages());
+
         // List<Ebook>需要转换成List<EbookResp>，再返回controller，需要使用循环
         // 具体的实现细节封装在了CopyUtil中
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
