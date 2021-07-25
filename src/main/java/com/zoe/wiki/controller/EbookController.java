fc71f8be27ccbd898718a750d14da90a2f8bd228
zoe <zoeeying@gmail.com>
2021-07-26 00:40:16 +0800

    后端参数校验

--- src/main/java/com/zoe/wiki/controller/EbookController.java
+++ src/main/java/com/zoe/wiki/controller/EbookController.java
@@ -9,6 +9,7 @@ import com.zoe.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/ebook")
@@ -17,7 +18,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(EbookQueryReq req){
+    public CommonResp list(@Valid EbookQueryReq req){
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> ebookList = ebookService.list(req);
         resp.setContent(ebookList);
--- src/main/java/com/zoe/wiki/req/PageReq.java
+++ src/main/java/com/zoe/wiki/req/PageReq.java
@@ -1,8 +1,14 @@
 package com.zoe.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Null;
+
 public class PageReq {
+    @Null(message = "page不能为空")
     private int page;
 
+    @Null(message = "size不能为空")
+    @Max(value = 1000, message = "size不能超过1000")
     private int size;
 
     public int getPage() {
