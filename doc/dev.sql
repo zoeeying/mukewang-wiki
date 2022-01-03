273b5a97e35122541e4c963f38e109c5bd236d9a
zoe <zoeeying@gmail.com>
2022-01-03 21:45:13 +0800

    mybatis demo

--- doc/dev.sql
+++ doc/dev.sql
@@ -2,7 +2,7 @@
 #<ebook表>#####################################################################
 drop table if exists `ebook`;
 create table `ebook` (
-  `id` bigint not null comment 'id',
+  `id` bigint not null auto_increment comment 'id',
   `name` varchar(50) comment '名称',
   `category1_id` bigint comment '分类1',
   `category2_id` bigint comment '分类2',
@@ -14,11 +14,11 @@ create table `ebook` (
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='电子书';
 
-insert into `ebook` (id, name, cover, description) values (1, 'Spring Boot 入门教程', '/image/cover1.png', '零基础入门 Java 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, cover, description) values (2, 'Vue 入门教程', '/image/cover2.png', '零基础入门 Vue 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (3, 'Python 入门教程', '零基础入门 Python 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用开发最佳首选框架');
-insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
+insert into `ebook` (id, name, cover, description) values (1, 'Spring Boot 入门教程', '/image/cover1.png', '是时候学习 Spring Boot 了！');
+insert into `ebook` (id, name, cover, description) values (2, 'Vue 入门教程', '/image/cover2.png', 'Vue 学到什么程度了啊？');
+insert into `ebook` (id, name, cover, description) values (3, 'Python 入门教程', '/image/cover2.png', '人工智能时代，你还不懂 Python 吗？');
+insert into `ebook` (id, name, cover, description) values (4, 'MySQL 入门教程', '/image/cover1.png', '数据库当然用 MySQL 啦！');
+insert into `ebook` (id, name, cover, description) values (5, 'Java 入门教程', '/image/cover1.png', 'Java 感觉也没有那么难嘛！');
 
 
 
--- src/main/java/com/zoe/wiki/domain/Test.java
+++ src/main/java/com/zoe/wiki/domain/Test.java
@@ -1,34 +1,14 @@
 package com.zoe.wiki.domain;
 
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
 public class Test {
     private Integer id;
 
     private String name;
 
     private String password;
-
-    // alt+insert快捷键生成Getter and Setter
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
